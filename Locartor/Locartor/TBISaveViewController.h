15d1338cdeb00e0c0ff0265ee694862328906d93
Nick Jaremek <nick13jaremek@gmail.com>
2014-04-05 17:48:43 +0200

    Create SaveViewController and XIB files

---
+++ Locartor/Locartor/TBISaveViewController.h
@@ -0,0 +1,13 @@
+//
+//  TBISaveViewController.h
+//  Locartor
+//
+//  Created by Nicolás Jaremek on 05/04/14.
+//  Copyright (c) 2014 thebitisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBISaveViewController : UIViewController
+
+@end
---
+++ Locartor/Locartor/TBISaveViewController.m
@@ -0,0 +1,38 @@
+//
+//  TBISaveViewController.m
+//  Locartor
+//
+//  Created by Nicolás Jaremek on 05/04/14.
+//  Copyright (c) 2014 thebitisland. All rights reserved.
+//
+
+#import "TBISaveViewController.h"
+
+@interface TBISaveViewController ()
+
+@end
+
+@implementation TBISaveViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
