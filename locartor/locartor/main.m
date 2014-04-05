7e01f7c59e2247f9bdcaf386827825b16fa723ae
manuvendev <manu.ventero@gmail.com>
2014-04-05 20:04:15 +0200

    Starting from Scratch

---
+++ locartor/locartor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  locartor
+//
+//  Created by Manu on 05/04/14.
+//  Copyright (c) 2014 thebitisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBIAppDelegate class]));
+    }
+}
